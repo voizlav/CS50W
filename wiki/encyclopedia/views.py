b1cc3b26dfff9c387342d9b5bd853e819f4272cb
Vojislav Trujkic <voizlav@gmail.com>
2023-04-01 06:56:44 +0200

    Add article to views

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,7 +4,10 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def article(request, title):
+    return render(
+        request, "encyclopedia/article.html", {"content": util.get_entry(title)}
+    )
