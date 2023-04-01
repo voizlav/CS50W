4bbb33dc280589e10ea7a8458cecbee4e8f907bf
Vojislav Trujkic <voizlav@gmail.com>
2023-04-01 08:22:29 +0200

    Add search functionality in view

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 
@@ -11,3 +12,16 @@ def article(request, title):
     return render(
         request, "encyclopedia/article.html", {"content": util.get_entry(title)}
     )
+
+
+def search(request):
+    articles = util.list_entries()
+    query = request.GET.get("q")
+    if not query:
+        return render(request, "encyclopedia/index.html", {"entries": articles})
+    if query in articles:
+        return HttpResponseRedirect(reverse("article", args=[query]))
+    result = []
+    for article in articles:
+        result.append(article) if query in article else None
+    return render(request, "encyclopedia/index.html", {"entries": result})
