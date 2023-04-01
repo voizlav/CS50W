b4c38abba67961f546b1aa2c44113886ad66eefd
Vojislav Trujkic <voizlav@gmail.com>
2023-04-01 08:37:37 +0200

    Update search to send bool

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -18,10 +18,14 @@ def search(request):
     articles = util.list_entries()
     query = request.GET.get("q")
     if not query:
-        return render(request, "encyclopedia/index.html", {"entries": articles})
+        return render(
+            request, "encyclopedia/index.html", {"entries": articles, "search": True}
+        )
     if query in articles:
         return HttpResponseRedirect(reverse("article", args=[query]))
     result = []
     for article in articles:
         result.append(article) if query in article else None
-    return render(request, "encyclopedia/index.html", {"entries": result})
+    return render(
+        request, "encyclopedia/index.html", {"entries": result, "search": True}
+    )
