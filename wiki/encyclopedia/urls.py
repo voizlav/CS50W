ac23d7ea0cd10e7a884eb2e2723b5ee16aa16379
Vojislav Trujkic <voizlav@gmail.com>
2023-04-01 08:21:55 +0200

    Add search path

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.article, name="article"),
+    path("search", views.search, name="search"),
 ]
