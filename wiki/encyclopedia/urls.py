31fc28cf610d569057a78c0e32506c80a2c5a0c1
Vojislav Trujkic <voizlav@gmail.com>
2023-04-02 06:59:28 +0200

    Add edit title path

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("random", views.random, name="random"),
     path("new", views.new, name="new"),
+    path("edit/<str:title>", views.edit, name="edit"),
 ]
