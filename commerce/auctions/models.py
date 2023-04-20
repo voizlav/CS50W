19ad458336d18dd33b86fbe10a045c37f86598f0
Vojislav Trujkic <voizlav@gmail.com>
2023-04-20 23:46:01 +0200

    Add auction model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Auction(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=1000)
+    starting_bid = models.IntegerField(default=0)
+    category = models.CharField(blank=True, max_length=30)
+    hyperlink = models.CharField(blank=True, max_length=200)
+
+
+class Bids:
+    # TODO
+    pass
+
+
+class Comments:
+    # TODO
+    pass
