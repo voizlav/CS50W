0d579d61a15fab2c4feb794e36a0b77ad0ec982c
Vojislav Trujkic <voizlav@gmail.com>
2023-05-12 23:48:52 +0200

    Add title and description

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -76,6 +76,7 @@ def create(request):
     if request.method == "POST":
         form = AuctionForm(request.POST)
         if form.is_valid():
-            ...
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
 
     return render(request, "auctions/create.html", {"form": AuctionForm()})
