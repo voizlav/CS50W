21337e3a9ec19e777c1a92ec474971cab09a57a2
Vojislav Trujkic <voizlav@gmail.com>
2023-05-15 21:16:30 +0200

    Add MaxValueValidator to starting bid

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
@@ -12,7 +12,9 @@ class Auction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=1000)
-    starting_bid = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    starting_bid = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(2147483647)]
+    )
     category = models.CharField(blank=True, max_length=30)
     hyperlink = models.URLField(blank=True, max_length=200)
     time = models.TimeField(default=timezone.now().time())
