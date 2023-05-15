ca891e9641c7edbbf25571e39310410ad39485d9
Vojislav Trujkic <voizlav@gmail.com>
2023-05-15 21:12:24 +0200

    Add MinValueValidator to starting bid

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 
 class User(AbstractUser):
@@ -11,7 +12,7 @@ class Auction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=1000)
-    starting_bid = models.IntegerField(default=0)
+    starting_bid = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     category = models.CharField(blank=True, max_length=30)
     hyperlink = models.URLField(blank=True, max_length=200)
     time = models.TimeField(default=timezone.now().time())
