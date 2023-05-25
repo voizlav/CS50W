d8a7a5ff646474bf33cc85fd04144f980a30f04f
Vojislav Trujkic <voizlav@gmail.com>
2023-05-26 00:50:31 +0200

    Add path to close item

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create", views.create, name="create"),
     path("item/<int:item_id>", views.items, name="item"),
+    path("item/<int:item_id>/close", views.close_item, name="close_item"),
 ]
