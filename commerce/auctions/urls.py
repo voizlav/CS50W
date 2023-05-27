3b1132f38408dc57cdbc4cd6dcbbbda942e25fde
Vojislav Trujkic <voizlav@gmail.com>
2023-05-27 07:22:13 +0200

    Add endpoint for bidding on item

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("item/<int:item_id>", views.items, name="item"),
     path("item/<int:item_id>/close", views.close_item, name="close_item"),
+    path("item/<int:item_id>/bid", views.bid_item, name="bid_item"),
 ]
