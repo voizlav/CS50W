84058b031b2d8ba20335e2b3c9412632b3f00c79
Vojislav Trujkic <voizlav@gmail.com>
2023-05-28 08:23:27 +0200

    Update Comments model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -49,10 +49,10 @@ class Bids(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
-class Comments:
+class Comments(models.Model):
     auction = models.ForeignKey(
         Auction, on_delete=models.CASCADE, related_name="comment"
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=1000)
+    content = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
