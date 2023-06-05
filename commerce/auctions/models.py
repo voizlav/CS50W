a1fd99d8ca32bc01eb9a076c029fae1005a53500
Vojislav Trujkic <voizlav@gmail.com>
2023-06-05 17:36:46 +0200

    Update time in all models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone, datetime_safe
+from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -30,14 +30,11 @@ class Auction(models.Model):
     )
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    starting_bid = models.IntegerField(
-        default=0, validators=[MinValueValidator(0), MaxValueValidator(2147483647)]
-    )
     category = models.CharField(blank=True, max_length=30, choices=CATEGORIES)
     hyperlink = models.URLField(
         default="/static/img/noimage.png", blank=True, max_length=200
     )
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(default=timezone.now)
     active = models.BooleanField(default=True)
 
 
@@ -47,7 +44,7 @@ class Bids(models.Model):
     amount = models.IntegerField(
         default=0, validators=[MinValueValidator(0), MaxValueValidator(2147483647)]
     )
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
 
 
 class Comments(models.Model):
@@ -56,7 +53,7 @@ class Comments(models.Model):
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
 
 
 class Watchlist(models.Model):
