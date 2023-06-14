e73f486f2333853ae312c767b95b26d571302d35
Vojislav Trujkic <voizlav@gmail.com>
2023-06-14 23:43:48 +0200

    Add Post model

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_post")
+    content = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # TODO likes
