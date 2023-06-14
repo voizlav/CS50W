c539d46aadb445ea5f75a3b20db336a89c4d161c
Vojislav Trujkic <voizlav@gmail.com>
2023-06-14 21:26:36 +0200

    Import login_required to views

--- project4/network/views.py
+++ project4/network/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 
