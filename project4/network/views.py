a79dc0f0e732b3abc3de9482bd8a6c5806cfecd1
Vojislav Trujkic <voizlav@gmail.com>
2023-06-14 21:29:53 +0200

    Import Json response to views

--- project4/network/views.py
+++ project4/network/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
