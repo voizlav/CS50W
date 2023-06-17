ef358d4eaf6d240b63f66031b9a63323798fa3cc
Vojislav Trujkic <voizlav@gmail.com>
2023-06-17 02:11:12 +0200

    Add createPostElements func separated from displayPosts

--- project4/network/static/network/main.js
+++ project4/network/static/network/main.js
@@ -15,51 +15,60 @@ const createNewPost = () => {
       body: JSON.stringify({ content: newPost.value }),
     })
       .then((res) => res.json())
-      .then((data) => data.message && (newPost.value = ""));
+      .then((data) => {
+        if (data.message) {
+          newPost.value = "";
+          displayPosts();
+        }
+      });
   };
 };
 
 const displayPosts = () => {
   const allPosts = document.querySelector("#allPosts");
   allPosts.classList.add("mt-5");
+  allPosts.innerHTML = "";
+
   fetch("/posts")
     .then((res) => res.json())
     .then((data) => {
       const listOfPosts = document.createElement("div");
       listOfPosts.classList.add("list-group");
-      data.forEach((post) => {
-        const listGroupPost = document.createElement("a");
-        listGroupPost.classList.add(
-          "list-group-item",
-          "list-group-item-action",
-        );
-        const divWrapper = document.createElement("div");
-        divWrapper.classList.add(
-          "d-flex",
-          "w-100",
-          "justify-content-between",
-          "pb-3",
-        );
-        const postHeading = document.createElement("h5");
-        postHeading.classList.add("mb-1");
-        postHeading.innerText = post.user;
-        const timestamp = document.createElement("small");
-        timestamp.classList.add("text-muted");
-        timestamp.innerText = post.timestamp;
-        const postContent = document.createElement("p");
-        postContent.classList.add("mb-1", "text-break");
-        postContent.innerText = post.content;
-
-        listGroupPost.appendChild(divWrapper);
-        divWrapper.appendChild(postHeading);
-        divWrapper.appendChild(timestamp);
-        listGroupPost.appendChild(postContent);
-        listOfPosts.appendChild(listGroupPost);
-
-        // TODO: Add likes
-      });
+      data.forEach((post) => listOfPosts.appendChild(createPostElements(post)));
       allPosts.appendChild(listOfPosts);
     });
+};
+
+const createPostElements = (post) => {
+  const listGroupPost = document.createElement("a");
+  listGroupPost.classList.add("list-group-item", "list-group-item-action");
+
+  const divWrapper = document.createElement("div");
+  divWrapper.classList.add(
+    "d-flex",
+    "w-100",
+    "justify-content-between",
+    "pb-3",
+  );
+  const postHeading = document.createElement("h5");
+  postHeading.classList.add("mb-1");
+  postHeading.innerText = post.user;
+
+  const timestamp = document.createElement("small");
+  timestamp.classList.add("text-muted");
+  timestamp.innerText = post.timestamp;
+
+  const postContent = document.createElement("p");
+  postContent.classList.add("mb-1", "text-break");
+  postContent.innerText = post.content;
+
+  listGroupPost.appendChild(divWrapper);
+  divWrapper.appendChild(postHeading);
+  divWrapper.appendChild(timestamp);
+  listGroupPost.appendChild(postContent);
+
+  // TODO: Add likes
+  return listGroupPost;
 };
 
 init();
