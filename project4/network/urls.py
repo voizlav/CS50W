738fcbde511e2c6ac016b4d17437b24e7f6bb3d9
Vojislav Trujkic <voizlav@gmail.com>
2023-06-18 03:07:58 +0200

    Add follow path to url patterns

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("login_status", views.login_status, name="login_status"),
     path("register", views.register, name="register"),
     path("newpost", views.newpost, name="newpost"),
     path("posts", views.posts, name="posts"),
-    path("login_status", views.login_status, name="login_status"),
+    path("follow", views.follow, name="follow"),
 ]
