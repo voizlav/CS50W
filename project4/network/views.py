44539595698148234ff5dbb179c1064485aef036
Vojislav Trujkic <voizlav@gmail.com>
2023-06-18 06:47:14 +0200

    Add import paginator to views

--- project4/network/views.py
+++ project4/network/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
 
 from .models import User, Post, Follow
 
